4415d9d868e8fa15a7c73aef9781f79f45381791
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-09-24 12:45:50 -0500

    agregar componente de card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
@@ -15,19 +16,7 @@ function App() {
       <div className='alert alert-primary' role='alert'>
         A simple primary alert—check it out!
       </div>
-      <div className='card'>
-        <img src='...' className='card-img-top' alt='...' />
-        <div className='card-body'>
-          <h5 className='card-title'>Card title</h5>
-          <p className='card-text'>
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </p>
-          <a href='#' className='btn btn-primary'>
-            Go somewhere
-          </a>
-        </div>
-      </div>
+      <Card />
     </div>
   );
 }
