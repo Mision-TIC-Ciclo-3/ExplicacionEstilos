fb0e1e804473d2ef78223ccad088927f205a42fd
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-09-24 13:35:05 -0500

    agregar explicacion de sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='fondo1'>Este es el ejemplo1</div>
+      <div className='fondo2'>Este es el ejemplo2</div>
+      <div className='fondo3'>Este es el ejemplo3</div>
     </div>
   );
 }
